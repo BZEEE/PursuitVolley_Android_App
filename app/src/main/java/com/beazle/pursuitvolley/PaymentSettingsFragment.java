e45c892057df19b7dbe1e849a566c2e251e1f1cb
bjzuk <bjzukows@ualberta.ca>
2019-07-20 12:18:03 -0400

    WIP, fixing coach user profile

--- app/src/main/java/com/beazle/pursuitvolley/PaymentSettingsFragment.java
+++ app/src/main/java/com/beazle/pursuitvolley/PaymentSettingsFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,16 +15,51 @@ public class PaymentSettingsFragment extends Fragment {
 
     private Button enterNewDirectDepositInfoButtonToggle;
     private Button viewCurrentDirectDepositInfoButtonToggle;
+    private RelativeLayout editDirectDepositInfoLayout;
+    private RelativeLayout coachCurrentDirectDepositInfoLayout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        enterNewDirectDepositInfoButtonToggle.setVisibility(View.INVISIBLE);
-        enterNewDirectDepositInfoButtonToggle.setEnabled(false);
-        viewCurrentDirectDepositInfoButtonToggle.setVisibility(View.VISIBLE);
-        viewCurrentDirectDepositInfoButtonToggle.setEnabled(true);
+        View paymentSettingFragmentView = inflater.inflate(R.layout.fragment_payment_settings, container, false);
 
-        return inflater.inflate(R.layout.fragment_payment_settings, container, false);
+        enterNewDirectDepositInfoButtonToggle = paymentSettingFragmentView.findViewById(R.id.enterNewDirectDepositInfoButtonToggle);
+        viewCurrentDirectDepositInfoButtonToggle = paymentSettingFragmentView.findViewById(R.id.viewCurrentDirectDepositInfoButtonToggle);
+        editDirectDepositInfoLayout = paymentSettingFragmentView.findViewById(R.id.editDirectDepositInfoLayout);
+        coachCurrentDirectDepositInfoLayout = paymentSettingFragmentView.findViewById(R.id.coachCurrentDirectDepositInfoLayout);
+
+        editDirectDepositInfoLayout.setVisibility(View.VISIBLE);
+        coachCurrentDirectDepositInfoLayout.setVisibility(View.INVISIBLE);
+
+        enterNewDirectDepositInfoButtonToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowDirectDepositInfoEditingLayout();
+            }
+        });
+
+        viewCurrentDirectDepositInfoButtonToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowCurrentDirectDepositInfoLayout();
+            }
+        });
+
+        return paymentSettingFragmentView;
+    }
+
+    private void ShowDirectDepositInfoEditingLayout() {
+        editDirectDepositInfoLayout.setVisibility(View.VISIBLE);
+        coachCurrentDirectDepositInfoLayout.setVisibility(View.INVISIBLE);
+        enterNewDirectDepositInfoButtonToggle.setBackgroundResource(R.drawable.rounded_corners_left_blue_background);
+        viewCurrentDirectDepositInfoButtonToggle.setBackgroundResource(R.drawable.rounded_corners_right_grey_background);
+    }
+
+    private void ShowCurrentDirectDepositInfoLayout() {
+        editDirectDepositInfoLayout.setVisibility(View.INVISIBLE);
+        coachCurrentDirectDepositInfoLayout.setVisibility(View.VISIBLE);
+        enterNewDirectDepositInfoButtonToggle.setBackgroundResource(R.drawable.rounded_corners_left_grey_background);
+        viewCurrentDirectDepositInfoButtonToggle.setBackgroundResource(R.drawable.rounded_corners_right_blue_background);
     }
 }
